e4a0bfacd04aaa5e9addd545188d89fa23faa07b
Meiyappan Annamalai KMA <meiyappanmeenal2005@gmail.com>
2025-06-01 22:20:45 +0530

    Add files via upload

---
+++ WD40/index.js
@@ -0,0 +1,17 @@
+//var generateName = require('sillyname');
+// import generateName from "sillyname";
+// var sillyName = generateName();
+
+// console.log(`My name is ${sillyName}.`);
+
+import generateName from "sillyName";
+
+var sillyName = generateName();
+
+console.log(`My name is ${sillyName}.`);
+
+// const superheroes = require("superheroes");
+import superheroes from "superheroes";
+
+const name = superheroes.random();
+console.log(`I am ${name}!`);
